1979737558afb8489748ffbdac25e52731a95c99
Juser0 <justuser0129@gmail.com>
2024-10-31 21:54:55 +0900

    chore: init projects

---
+++ src/main/kotlin/com/military/recruit/RecruitApplication.kt
@@ -0,0 +1,11 @@
+package com.military.recruit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecruitApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecruitApplication>(*args)
+}
---
+++ src/test/kotlin/com/military/recruit/RecruitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.military.recruit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecruitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
